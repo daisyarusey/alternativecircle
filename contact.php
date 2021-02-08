6c3ac21bcdafcc88f597b247c14ee1a4625ffac9
daisyarusey <41824509+daisyarusey@users.noreply.github.com>
2021-02-08 10:03:11 +0300

    Change the contact form

--- contact.php
+++ contact.php
@@ -1,21 +1,17 @@
 <?php
-if(!isset($_REQUEST['username']) || empty($_REQUEST['username'])){
-	print_r(json_encode(array('status'=>0, 'message' => 'Please enter your name'))); exit;
-}
-//validate email
-if(!isset($_REQUEST['email']) || !filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)){
-	print_r(json_encode(array('status'=>0, 'message' => 'Please enter a valid email address'))); exit;
-}
+$name = $_REQUEST['username'];
+$email= $_REQUEST['email'];
+$subject= $_REQUEST['subject'];
+$message=$_REQUEST['message'];
 
-//validate subject
-if(!isset($_REQUEST['subject']) || empty($_REQUEST['subject'])){
-	print_r(json_encode(array('status'=> 0, 'message' => 'Please enter a subject for your message'))); exit;
+if(empty($name) || empty($email) || empty($subject) || empty($message)){
+	echo("Please fill all the fields");
 }
+else{
+	mail("daisyarusey.dc@gmail.com",$subject,"From: $name <$email>");
+	echo"<script type='text/javascript' >alert('your message sent successfully.)</script>";
 
-//validate message
-if(!isset($_REQUEST['message']) || empty($_REQUEST['message'])){
-	print_r(json_encode(array('status'=> 0, 'message' => 'Please enter your message'))); exit;
+	window.history.log(-1);
 }
 
-print_r(json_encode(array('status'=> 1, 'message' => 'Your message has been sent to our team. We will get back to you within 12 hours'))); exit;
-
+?>
