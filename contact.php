51805d5be24aead080cf6eac8c221f3f2058a7dc
daisyarusey <41824509+daisyarusey@users.noreply.github.com>
2021-02-09 16:55:42 +0300

    Change bg image

--- contact.php
+++ contact.php
@@ -1,17 +1,48 @@
 <?php
-$name = $_REQUEST['username'];
-$email= $_REQUEST['email'];
-$subject= $_REQUEST['subject'];
-$message=$_REQUEST['message'];
-
-if(empty($name) || empty($email) || empty($subject) || empty($message)){
-	echo("Please fill all the fields");
+$errors = '';
+$myemail = 'daisyarusey.dc@gmail.com';
+if(empty($_POST['username'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['subject']) ||
+   empty($_POST['message']))
+{
+    // print_r('no records passed');die;
+    $errors .= "\n Error: all fields are required";
+} else {
+    
 }
-else{
-	mail("daisyarusey.dc@gmail.com",$subject,"From: $name <$email>");
-	echo"<script type='text/javascript' >alert('your message sent successfully.)</script>";
+$name = $_POST['username'];
+$email_address = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-	window.history.log(-1);
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
 }
 
+if( empty($errors))
+{
+   
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email_address\n \n Subject $subject Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+
+ini_set();
+ini_set();
+
+// print_r($email_body); die;
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+print_r(json_encode(array('status'=> 1, 'message' => 'Your message has been sent to our team. We will get back to you within 12 hours')));
+
+} else {
+    print_r('errors available'); die;
+ }
 ?>
